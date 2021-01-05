1c52b360797a2ec36a5024d30af7f5b958e8b206
noondev <andreicriste15@yahoo.ro>
2021-01-05 11:42:26 +0200

    Add project files.

---
+++ Criste_Andrei_Lab12/Criste_Andrei_Lab12/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Criste_Andrei_Lab12.Data;
+
+namespace Criste_Andrei_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+
+        public App()
+        {
+            /* InitializeComponent();
+
+             MainPage = new MainPage();*/
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
